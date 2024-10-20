4e9f4c73df83521b4df619c245ccad53f8a51ab5
Voidknight-2004 <rohanajacobgdsc@gmail.com>
2024-10-20 13:48:53 +0530

    added dropdown for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,13 +7,21 @@ import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ sidebarToggle, setSidebarToggle }) => {
   const [profile, setProfile] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
   const userToken = Cookies.get("token");
   const { logout } = useAuth();
   const navigate = useNavigate();
+  const userRoles = userToken ? jwtDecode(userToken).roles : [];
 
   useEffect(() => {
-    setProfile(jwtDecode(userToken).username);
-  }, []);
+    if (userToken) {
+      setProfile(jwtDecode(userToken).username);
+    }
+  }, [userToken]);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -22,7 +30,7 @@ const Navbar = ({ sidebarToggle, setSidebarToggle }) => {
   };
 
   return (
-    <nav className="bg-gray-800 px-4 py-3 flex justify-between  ">
+    <nav className="bg-gray-800 px-4 py-3 flex justify-between">
       <div className="flex items-center text-xl">
         <FaBars
           className="text-white me-4 cursor-pointer"
@@ -30,26 +38,54 @@ const Navbar = ({ sidebarToggle, setSidebarToggle }) => {
         />
         <span className="text-white font-semibold">User management</span>
       </div>
-      <div className="flex items-center  gap-x-5 ">
+      <div className="flex items-center gap-x-5">
         <div className="relative flex items-center justify-center">
-          <h1 className="mr-3 ">{profile}</h1>
-          <button className="text-white group">
-            <FaUserCircle className="w-6 h-6 mt-1" />
-
-            <div className="z-10 hidden absolute rounded-lg bg-white shadow w-32 group-focus:block top-full right-0">
+          <h1 className="mr-3 text-white">{`Welcome ${profile}`}</h1>
+          <div className="relative group">
+            <button className="text-white">
+              <FaUserCircle className="w-6 h-6 mt-1 mr-4" />
+            </button>
+            <div className="z-10 hidden absolute rounded-lg bg-white shadow w-32 group-hover:block top-full right-0">
               <ul className="flex-col py-2 text-sm text-gray-950">
-                {/* <li>
-                  <a href="">Profile</a>
-                </li>
                 <li>
-                  <a href="">Setting</a>
-                </li> */}
-                <li >
-                  <p onClick={handleLogout}>Logout</p>
+                  <p
+                    onClick={handleLogout}
+                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  >
+                    Logout
+                  </p>
                 </li>
               </ul>
             </div>
-          </button>
+          </div>
+          <div className="relative">
+            <select
+              id="select-menu"
+              className="block appearance-none w-28 h-7 bg-white border border-gray-300 hover:border-gray-400 pr-4 text-center pr-8 text-gray-700 text-sm focus:ring-blue-500 focus:border-blue-500 rounded-md"
+              value={selectedOption}
+              onChange={handleOptionChange}
+            >
+              {userRoles.map((role, index) => (
+                <option key={index} value={role}>
+                  {role}
+                </option>
+              ))}
+            </select>
+            <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import userModel from "../postgresql/schema/userSchema.js";
 import roleModel from "../postgresql/schema/rolesSchema.js";
-import permissionModel from "../postgresql/schema/permissionSchema.js";
+import bcrypt from "bcrypt";
 
 const SECRET_KEY = "f27om2feQYLKQZl6uBkw";
 
@@ -16,7 +16,10 @@ const signup = async (req, res) => {
     return res.status(409).send("User already exists");
   } else {
     try {
-      const user = await userModel.create({ username: username, password: password });
+      const user = await userModel.create({
+        username: username,
+        password: password,
+      });
       const role = await roleModel.findOne({
         where: {
           role: "default",
@@ -42,14 +45,14 @@ const signin = async (req, res) => {
   const isMatch = await bcrypt.compare(password, existingUser.password);
 
   if (isMatch && existingUser) {
-    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "10h" });
+    const roleInstance = await existingUser.getRoles();
+    const roleArray = roleInstance.map(roleInstance => roleInstance.dataValues.role);
+    const token = jwt.sign({ username,roles:roleArray }, SECRET_KEY, { expiresIn: "10h" });
     res.status(201).json({ token });
   } else {
     res.status(409).send("Invalid credentials");
   }
 };
-
-await userModel.sync({ alter: true });
 
 const userController = {
   signup,
